a2239f5f31e8312017029127678b91c89341887b
Grg0rry <19084193@imail.sunway.edu.my>
2023-06-21 16:12:33 +0800

    update the code

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import itertools
+import random
 
 
 # Initiate webdriver
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
 driver.get('https://izone.sunway.edu.my/login')
 time.sleep(5)
 
-# Credentials
+# Credentials --replace with name and password
 student_id = ''
 password = ''
 
 # Logging in
-login_username = driver.find_element_by_id('student_uid')
-login_password = driver.find_element_by_id('password')
+login_username = driver.find_element('id', 'student_uid')
+login_password = driver.find_element('id', 'password')
 login_username.clear()
 login_password.clear()
 login_username.send_keys(student_id)
 login_password.send_keys(password)
-driver.find_element_by_id('submit').click()
+driver.find_element('id', 'submit').click()
+time.sleep(5)
 
 # Checkin
-driver.find_element_by_id('icheckin').click()
-checkin = driver.find_element_by_id('checkin_code')
+driver.find_element('id', 'iCheckInUrl').click()
 
-combinations = list(itertools.permutations(range(10), 5))
-for combination in combinations:
+# Generate all possible combinations of 5 digit code
+combinations = list(itertools.product(range(10), repeat=5))
+result = [''.join(map(str, combo)) for combo in combinations]
+random.shuffle(result)
+
+# Loop until checkin code found/successful
+start = time.time()
+for combination in result:
+    driver.execute_script("window.scrollBy(0, 100);")
+    checkin = driver.find_element('id', 'checkin_code')
+    checkin.clear()
     checkin.send_keys(combination)
-    driver.find_element_by_id('iCheckin').click()
-    notif = driver.find_element_by_id('notification').get_attribute('class')
+    driver.find_element('id', 'iCheckin').click()
+    notif = driver.find_element('id', 'notification').get_attribute('class')
     if 'alert-success' in notif:
         print(f'Checkin Code: {combination}')
+        print('Successfully Checkedin!!!')
         break
 
-print('Successfully Checkedin!!!')
+end = time.time()
+time_taken = end - start
+print(f'time taken: {time_taken}')
 
 # Close the browser
 driver.quit()
